85c9f78510b1bfec9959e6156379482da28bff48
Andrei Kuzmin <andrei.kuzmin@godeltech.com>
2021-03-26 17:58:28 +0300

    used baseUrl for navigation to be under portfolio project

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,16 +1,18 @@
 import './style.css';
 
 const Nav = () => {
+  const baseUrl = process.env.PUBLIC_URL;
+
   return (
     <nav>
       <div className="logo">
         <h4>Andrei Kuzmin</h4>
       </div>
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/work">Work</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/projects">Projects</a></li>
+        <li><a href={`${baseUrl}/`}>Home</a></li>
+        <li><a href={`${baseUrl}/work`}>Work</a></li>
+        <li><a href={`${baseUrl}/about`}>About</a></li>
+        <li><a href={`${baseUrl}/projects`}>Projects</a></li>
       </ul>
       <div className="burger">
         <div></div>
